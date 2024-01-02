299e7394c486aa5639ddd9a4a9c691c8cc6352e9
Ruma303 <matteoh8@hotmail.it>
2024-01-02 21:06:39 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,90 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // --- Operatori Unari
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        // -- Incremento e decremento
+        int x = 1; System.out.println(x); // 1
+        System.out.println(++x); // 2
+        System.out.println(x++); // 2
+        System.out.println(x); // 3
+        System.out.println(--x); // 2
+        System.out.println(x--); // 2
+        System.out.println(x); // 1
+
+
+        // -- Positivo unario
+        int positive = +5; System.out.println(positive); // 5
+
+        // -- Negativo unario
+        int negative = -5; System.out.println(negative); // -5
+
+
+        // --- Operatori aritmetici
+        System.out.println(5 + 3); // 8
+        System.out.println(5 - 3); // 2
+        System.out.println(5 * 3); // 15
+        System.out.println(15.0 / 3.2); // 4.6875
+        System.out.println(10 % 4); // 2
+
+
+        // --- Operatori shift
+        int e = 2;
+        System.out.println(e << 1); // 4, in binario: 100
+
+        int f = 4;
+        System.out.println(f << 1); // 2, in binario: 10
+
+        int g = -4;
+        System.out.println(g >>> 1); //
+
+
+        // --- Operatori di confronto
+        System.out.println(5 < 10); // true
+        System.out.println(5 > 3); // false
+        System.out.println(5 <= 3); // true
+        System.out.println(5 >= 10); // false
+        System.out.println(5 == 5); // true
+        System.out.println(5 != 10); // true
+
+
+
+        // --- Operatori bitwise
+        System.out.println(5 & 3); // 1
+        System.out.println(5 | 3); // 7
+        System.out.println(5 ^ 3); // 6
+        System.out.println(~5); // -6
+
+
+
+        // --- Operatori logici
+        System.out.println(true && false); // false
+        System.out.println(true || false); // true
+        System.out.println(!true); // false
+
+
+
+        // --- Short-circuit evaluation
+        System.out.println(0 !=0 && 10 / 0 > 1); // false
+
+
+        // --- Operatori di assegnamento
+
+        // -- Operatori aritmetici
+        int a = 5;
+        System.out.println(a += 5); // 10
+        System.out.println(a -= 5); // 5
+        System.out.println(a *= 5); // 25
+        System.out.println(a /= 5); // 5
+        System.out.println(a %= 5); // 1
+
+
+        // -- Operatori aritmetici
+        int b = 5;
+        System.out.println(b &= 5); // 0
+        System.out.println(b |= 5); // 5
+        System.out.println(b <<= 5); // 160
+        System.out.println(b >>= 5); // 5
+        System.out.println(b >>>= 5); // 0
     }
 }
